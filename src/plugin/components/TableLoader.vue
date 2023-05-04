652287bb8429656a50b8f791812bb043da180305
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-03 19:58:01 -0700

    Building out table loader more. Adding more options

--- src/plugin/components/TableLoader.vue
+++ src/plugin/components/TableLoader.vue
@@ -1,73 +1,72 @@
 <template>
-	<div
+	<v-row
 		v-if="loading"
 		:class="containerClasses"
+		no-gutters
 	>
-		<v-progress-linear
-			v-if="linear"
-			:color="linearColor"
-			:height="height"
-			indeterminate
-		></v-progress-linear>
-
-		<v-skeleton-loader
-			v-if="skelton"
-			:loading="loading"
-			:type="skeltonType"
+		<v-col
+			v-if="loaderType('linear')"
+			class="pa-0 ma-0"
+			:order="getOrder('linear')"
 		>
-		</v-skeleton-loader>
+			<v-progress-linear
+				:color="linearColor"
+				:height="height"
+				indeterminate
+			></v-progress-linear>
+		</v-col>
 
-		<v-progress-circular
-			v-if="circular"
+		<v-col
+			v-if="loaderType('circular')"
+			class="pa-0 my-2"
+			:order="getOrder('circular')"
+		>
+			<v-progress-circular
+				:bg-color="circularBackgroundColor"
+				:color="circularColor"
+				indeterminate
+				:size="size"
+			></v-progress-circular>
+		</v-col>
+
+		<v-col
+			v-if="loaderType('skelton')"
+			class="pa-0 ma-0"
+			:order="getOrder('skelton')"
+		>
+			<v-skeleton-loader
+				:loading="loading"
+				:type="skeltonType"
+			>
+			</v-skeleton-loader>
+		</v-col>
+
+		<v-col
+			v-if="loaderType('text')"
 			class="my-2"
-			:color="circularColor"
-			indeterminate
-			:size="size"
-		></v-progress-circular>
-
-		<div
-			v-if="textLoader"
-			class="py-2"
-			:class="`text-${textColor}`"
+			:order="getOrder('text')"
+			:style="textStyles"
 		>
 			{{ computedLoadingText }}
-		</div>
-	</div>
+		</v-col>
+	</v-row>
 </template>
 
 <script setup lang="ts">
+import * as DrilldownTypes from '@/types';
 import { componentName } from '@/plugin/utils/globals';
+import { useGetLevelColors } from '@/plugin/composables/levelColors';
+const theme = useTheme();
 
 const props = defineProps({
-	circular: {
-		default: true,
-		required: false,
-		type: Boolean,
-	},
-	circularColor: {
-		default: 'primary',
-		required: false,
-		type: String,
-	},
-	color: {
-		default: 'primary',
-		required: false,
-		type: String,
-	},
 	height: {
 		default: '2',
 		required: false,
 		type: String,
 	},
-	linear: {
-		default: true,
-		required: false,
-		type: Boolean,
-	},
-	linearColor: {
-		default: 'surface-variant',
-		required: false,
-		type: String,
+	loadedDrilldown: {
+		required: true,
+		type: Object as PropType<DrilldownTypes.LoadedDrilldown>,
 	},
 	loading: {
 		required: true,
@@ -83,21 +82,6 @@ const props = defineProps({
 		required: false,
 		type: String,
 	},
-	skelton: {
-		default: true,
-		required: false,
-		type: Boolean,
-	},
-	skeltonType: {
-		default: 'heading@1',
-		required: false,
-		type: String,
-	},
-	textColor: {
-		default: 'surface-variant',
-		required: false,
-		type: String,
-	},
 	textLoader: {
 		default: true,
 		required: false,
@@ -105,19 +89,75 @@ const props = defineProps({
 	}
 });
 
+const baseColors = useGetLevelColors(props.loadedDrilldown, theme, 'loader');
+
 
 const containerClasses = computed(() => {
 	return {
 		[`${componentName}--table-loader`]: true,
-		'text-center': true,
-		'pa-0': true,
+		'align-center': false,
+		'd-grid': false,
+		'flex-column': true,
 		'ma-0': true,
+		'pa-0': true,
+		'text-center': true,
 	};
 });
 
-const computedLoadingText = computed(() => {
+const skeltonType = computed<string>(() => {
+	return props.loadedDrilldown.skeltonType || 'heading@1';
+});
+
+
+const circularColor = computed<string | undefined>(() => {
+	return baseColors.circular;
+});
+
+const circularBackgroundColor = computed<string | undefined>(() => {
+	return baseColors.bg;
+});
+
+const linearColor = computed<string | undefined>(() => {
+	return baseColors.linear;
+});
+
+const textStyles = computed<StyleValue>(() => {
+	const styles = {
+		color: baseColors.text,
+	};
+
+	return styles;
+});
+
+const computedLoadingText = computed<string>(() => {
 	return props.loadingText || 'Loading...';
 });
+
+const getOrder = (type: string): number => {
+	const loaderType = props.loadedDrilldown.loaderType;
+
+	if (Array.isArray(loaderType)) {
+		return loaderType.indexOf(type);
+	}
+
+	return 1;
+};
+
+
+// Check if the loader type is enabled //
+const loaderType = (type: string): boolean => {
+	const loaderType = props.loadedDrilldown.loaderType;
+
+	if (type === props.loadedDrilldown.loaderType) {
+		return true;
+	}
+
+	if (Array.isArray(loaderType)) {
+		return loaderType.includes(type);
+	}
+
+	return false;
+};
 </script>
 
 
