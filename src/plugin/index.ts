bedc610e969f5ae071306e9a7603f0e49f9c23a6
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-05-12 11:26:35 -0700

    Changing back

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,7 +1,4 @@
 import VDrilldownTable from '@/plugin/VDrilldownTable.vue';
-import VDrilldownServerTable from '@/plugin/VDrilldownServerTable.vue';
 
-export {
-	VDrilldownTable,
-	VDrilldownServerTable,
-};
+
+export default VDrilldownTable;
