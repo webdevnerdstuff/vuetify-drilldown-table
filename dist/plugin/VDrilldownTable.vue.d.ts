b2a8ae8595e67878e70ab97bb33df724f56d1e33
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2023-06-08 19:51:27 -0700

    Building

--- dist/plugin/VDrilldownTable.vue.d.ts
+++ dist/plugin/VDrilldownTable.vue.d.ts
@@ -1,15 +1,13 @@
-declare const _sfc_main: import("vue").DefineComponent<any, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, ("update:drilldown" | "update:expanded" | "click:row" | "click:row:checkbox" | "update:drilldown:sortby" | "update:options" | "update:itemsPerPage" | "update:page" | "update:search" | "update:sortBy" | "update:sortByCustom")[], "update:drilldown" | "update:expanded" | "click:row" | "click:row:checkbox" | "update:drilldown:sortby" | "update:options" | "update:itemsPerPage" | "update:page" | "update:search" | "update:sortBy" | "update:sortByCustom", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<any> & {
+declare const _sfc_main: import("vue").DefineComponent<any, {}, unknown, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, ("update:drilldown" | "update:expanded" | "click:row" | "click:row:checkbox" | "update:options" | "update:itemsPerPage" | "update:page" | "update:search" | "update:sortBy")[], "update:drilldown" | "update:expanded" | "click:row" | "click:row:checkbox" | "update:options" | "update:itemsPerPage" | "update:page" | "update:search" | "update:sortBy", import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<any> & {
     "onClick:row"?: ((...args: any[]) => any) | undefined;
     "onUpdate:drilldown"?: ((...args: any[]) => any) | undefined;
     "onUpdate:expanded"?: ((...args: any[]) => any) | undefined;
     "onClick:row:checkbox"?: ((...args: any[]) => any) | undefined;
-    "onUpdate:drilldown:sortby"?: ((...args: any[]) => any) | undefined;
     "onUpdate:options"?: ((...args: any[]) => any) | undefined;
     "onUpdate:itemsPerPage"?: ((...args: any[]) => any) | undefined;
     "onUpdate:page"?: ((...args: any[]) => any) | undefined;
     "onUpdate:search"?: ((...args: any[]) => any) | undefined;
     "onUpdate:sortBy"?: ((...args: any[]) => any) | undefined;
-    "onUpdate:sortByCustom"?: ((...args: any[]) => any) | undefined;
 }, {} | {
     [x: string]: any;
 }, {}>;
