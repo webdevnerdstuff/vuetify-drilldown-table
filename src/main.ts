60b8343d57036edca38915c4e673520ef3d2779a
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-01-05 11:23:22 -0800

    Changed to use plugin version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import '@/libraries/fontawesome';
 import App from './App.vue';
 import CodeBlock from '@wdns/vue-code-block';
-import VDrilldownTable from './index';
+import { createVDrilldownTable } from './plugin/index';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { makeServer } from './server';
@@ -11,7 +11,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 makeServer({ environment: 'demo' });
 
 const app = createApp(App);
-app.use(VDrilldownTable);
+app.use(createVDrilldownTable());
 app.component('CodeBlock', CodeBlock);
 app.use(createPinia());
 app.component('font-awesome-icon', FontAwesomeIcon);
