d94c40140bf1883ce69263834a9d35cd32b6d8d4
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-03-13 13:18:41 -0700

    Build

---
+++ dist/VDrilldownTable-CB1KOdXg.js
@@ -0,0 +1,10 @@
+"use strict";/**
+ * @name @wdns/vuetify-drilldown-table
+ * @version 1.1.4
+ * @description The Vuetify Drilldown Table is a powerful component that enhances the functionality of the Vuetify framework's VDataTable and VDataTableServer. It provides a recursive table structure, allowing you to display hierarchical data in a nested format.
+ * @author WebDevNerdStuff & Bunnies... lots and lots of bunnies! <webdevnerdstuff@gmail.com> (https://webdevnerdstuff.com)
+ * @copyright Copyright 2024, WebDevNerdStuff
+ * @homepage https://webdevnerdstuff.github.io/vuetify-drilldown-table/
+ * @repository https://github.com/webdevnerdstuff/vuetify-drilldown-table
+ * @license MIT License
+ */Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("./vuetify-drilldown-table.cjs.js");exports.default=e.default;
